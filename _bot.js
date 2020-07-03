b409c2081965cd74d22fa6a706783cdce65aebc8
dramex <ea@ea.sa>
2020-07-03 05:56:13 +0300

    languages

--- _bot.js
+++ _bot.js
@@ -1,1 +1,22 @@
-module.exports = { "hello" : "hello2" }
+const languages = {};
+languages.ar = require("./ar-SA/bot.json");
+languages.bg = require("./bg-BG/bot.json");
+languages.de = require("./de-DE/bot.json");
+languages.el = require("./el-GR/bot.json");
+languages.en = require("./en-US/bot.json");
+languages.es = require("./es-ES/bot.json");
+languages.fr = require("./fr-FR/bot.json");
+languages.hu = require("./hu-HU/bot.json");
+languages.it = require("./it-IT/bot.json");
+languages.ja = require("./ja-JP/bot.json");
+languages.nl = require("./nl-NL/bot.json");
+languages.pl = require("./pl-PL/bot.json");
+languages.pt = require("./pt-PT/bot.json");
+languages.ro = require("./ro-RO/bot.json");
+languages.ru = require("./ru-RU/bot.json");
+languages.tr = require("./tr-TR/bot.json");
+languages.uk = require("./uk-UA/bot.json");
+languages.zh = require("./zh-CN/bot.json");
+languages["zh-TW"] = require("./zh-CN/bot.json");
+
+module.exports = languages;
